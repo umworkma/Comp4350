d9387ab4f469946912e79d696397a3f6a0ff9be3
William Sung <umsung@cc.umanitoba.ca>
2013-02-05 14:56:32 -0600

    Remove tutorial codes

--- ESA/application.py
+++ ESA/application.py
@@ -4,21 +4,6 @@ from ESA import app
 @app.route('/')
 def home():
     return render_template('index.html')
-    # return "Hello there" + data
-    # return app.config['ENV']
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    session['username'] = request.form['username']
-    session['message'] = request.form['message']
-    return redirect(url_for('message'))
-
-@app.route('/message')
-def message():
-    if not username in session:
-        return abort(403)
-    return render_template('message.html', username=session['username'], 
-                                           message=session['message'])
 
 if __name__ == '__main__':
     app.run()
