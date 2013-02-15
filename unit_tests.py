c5dae9248161023a12bc01933e2cbda90951749d
Chris Workman <umworkma@cc.umanitona.ca>
2013-02-15 08:50:18 -0600

    Added an actual test to unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,12 +1,22 @@
 import os
 import unittest
-from ESA import app
-from ESA.model import database
+import tempfile
+import ESA
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.db_fd, ESA.app.config['DATABASE'] = tempfile.mkstemp()
+        ESA.app.config['TESTING'] = True
         self.app = ESA.app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(ESA.app.config['DATABASE'])
+
+    def test_main_page(self):
+        rv = self.app.get('/')
+        assert 'ESA Service' in rv.data
         
 if __name__ == "__main__":
     unittest.main()
