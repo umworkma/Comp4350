a2e1c82820747647cd1bfc1daf96cb177625dc43
Chris Workman <umworkma@cc.umanitona.ca>
2013-02-15 20:43:20 -0600

    Update to database code to allow dependency injection

--- ESA/application.py
+++ ESA/application.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, session
 from ESA import app
 
+import config
+import models
+
+app.config.from_object(config)
+
+db = models.init_app(app)
+
 @app.route('/')
 def home():
     return render_template('index.html')
--- ESA/models.py
+++ ESA/models.py
@@ -1,33 +1,44 @@
-from sqlalchemy import *
-from sqlalchemy.orm import mapper, relation
-
-from database import DbInstance
-
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 # Globals
 TYPE_ORGANIZATION   = 1
 TYPE_EMPLOYEE       = 2
 
 # DB Initialization
-database = DbInstance()
+db = SQLAlchemy()
 
+def init_app(app):
+    """Initializes Flassk app."""
+    db.app = app
+    db.init_app(app)
+    return db
 
-# Link variables to DB tables via SQLAlchemy.
-entities      = Table('entity', database.metadata, autoload=True)
-organizations = Table('organization', database.metadata, autoload=True)
-addresses     = Table('address', database.metadata, autoload=True)
-
-
+def create_tables(app):
+    "Create tables, and return engine"
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    db.metadata.create_all(engine)
+    return engine
 
 # Class definition
-class Entity(object):
+class Entity(db.Model):
+    __tablename__ = 'entity'
+    pk = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Integer)
+
     def __repr__(self):
         return "<Entity('%s','%s')>" % (self.pk, self.type)
 
 class Organization(object):
+    __tablename__ = 'organization'
+    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk), primary_key=True)
+    name = db.Column(db.String(45))
+    description = db.Column(db.String(500))
+
     def __init__(self, name=None, description=None):
         self.name = name
         self.description = description
+
     def __repr__(self):
         return "<Organization('%s','%s','%s')>" % (self.entityFK, self.name, self.description)
 
@@ -35,6 +46,18 @@ class Organization(object):
         return database.session.query(Organization).order_by(Organization.name)
 
 class Address(object):
+    __tablename__ = 'address'
+    pk = db.Column(db.Integer, primary_key=True)
+    address1 = db.Column(db.String(45))
+    address2 = db.Column(db.String(45))
+    address3 = db.Column(db.String(45))
+    city = db.Column(db.String(45))
+    province = db.Column(db.String(45))
+    country = db.Column(db.String(45))
+    postalcode = db.Column(db.String(45))
+    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk))
+    isprimary = db.Column(db.Integer)
+    
     def __init__(self, address1=None, address2=None, address3=None, city=None,
                  province=None, country=None, postalcode=None, isprimary=None):
         self.address1   = address1
@@ -45,19 +68,10 @@ class Address(object):
         self.country    = country
         self.postalcode = postalcode
         self.isprimary  = isprimary
+        
     def __repr__(self):
         return "<Address('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')>" % (self.pk, self.entityFK, self.address1, self.address2, self.address3, self.city, self.province, self.country, self.postalcode, self.isprimary)
 
     def getByEntity(self, entityid):
         return database.session.query(Address).filter_by(entityFK=entityid)
 
-
-
-# Relationships
-entitymapper = mapper(Entity, entities)
-organizationmapper = mapper(Organization, organizations, properties={
-    'entity': relation(Entity, backref='organization'),
-})
-addressmapper = mapper(Address, addresses, properties={
-    'entity': relation(Entity, backref='addresses'),
-})
