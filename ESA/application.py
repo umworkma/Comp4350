bd91adb13a9998c0668ff4214af81bf5060d3b2b
Diana <diana@diana-Aspire-one.(none)>
2013-02-18 15:42:25 -0600

    Committing so I can pull

--- ESA/application.py
+++ ESA/application.py
@@ -13,8 +13,22 @@ def home():
     return render_template('index.html')
 
 @app.route('/register_organization.html/')
-def home2():
+def register_organization():
     return render_template('register_organization.html')
+
+@app.route('/_submit_org_form')
+def submit_org_form():
+    #get all user information
+    orgname = request.args.get('inputName')
+    description = request.args.get('inputDescription')
+    phone = request.args.get('inputPhone')
+    address = request.args.get('inputAddress')
+    city = request.args.get('inputCity')
+    province = request.args.get('inputProvince')
+    postal = request.args.get('inputPostal')
+    email = request.args.get('inputEmail')
+    #create a jason object and send it back to client 
+    return jsonify(orgname=orgname, description=description, phone=phone, address=address, city=city, province=province, postal=postal, email=email)
 
 @app.teardown_request
 def shutdown_session(exception=None):
