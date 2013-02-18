35b379d180a4414aee3d39b44275f8af8967a90f
Chris Workman <umworkma@cc.umanitona.ca>
2013-02-18 11:25:06 -0600

    Updated models further to include cascading deletes. Included additional tests to test adding and deleting data from the database

--- ESA/models.py
+++ ESA/models.py
@@ -28,8 +28,8 @@ class Entity(db.Model):
     __tablename__ = 'entity'
     pk = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Integer)
-    addresses = db.relationship('Address')
-    contacts = db.relationship('Contact')
+    addresses = db.relationship('Address', cascade='all, delete')
+    contacts = db.relationship('Contact', cascade='all, delete')
     
     def __init__(self, type=None):
         self.type = type
@@ -46,7 +46,7 @@ class Address(db.Model):
     city = db.Column(db.String(45))
     province = db.Column(db.String(45))
     postalcode = db.Column(db.String(45))
-    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk))
+    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk, ondelete='cascade'))
     isprimary = db.Column(db.Boolean)
 
     def __init__(self, address1=None, address2=None, address3=None, city=None, province=None, postalcode=None, entityFK=None, isprimary=None):
@@ -65,7 +65,7 @@ class Address(db.Model):
 class Contact(db.Model):
     __tablename__ = 'contact'
     pk = db.Column(db.Integer, primary_key=True)
-    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk))
+    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk, ondelete='cascade'))
     type = db.Column(db.Integer)
     value = db.Column(db.String(45))
     isprimary = db.Column(db.Boolean)
@@ -75,10 +75,10 @@ class Contact(db.Model):
     
 class Organization(db.Model):
     __tablename__ = 'organization'
-    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk), primary_key=True)
+    entityFK = db.Column(db.Integer, db.ForeignKey(Entity.pk, ondelete='cascade'), primary_key=True)
     name = db.Column(db.String(45))
     description = db.Column(db.Text)
-    entity = db.relationship('Entity',uselist=False)
+    entity = db.relationship('Entity',uselist=False, cascade='all, delete')
 
     def __repr__(self):
         return "<Organization('%s','%s','%s')>" % (self.EntityFK, self.name, self.description)
--- ESA/unit_tests.py
+++ ESA/unit_tests.py
@@ -72,6 +72,67 @@ class ESATestCase(TestCase):
     def test_entity_contact_relationship(self):
         entity1 = models.Entity.query.first()
         self.assertEquals(entity1.contacts[0].value, 'info@ai-kon.org')
+
+    """ Test adding a complete organization to the database """
+    def test_add_organization(self):
+        """ Define the data objects to be added """
+        entity1 = models.Entity(type=models.TYPE_ORGANIZATION)
+        address1 = models.Address(address1='4350 University Drive', address2='Suite 350',
+                           city='Winnipeg', province='Manitoba', entityFK=entity1.pk,
+                           isprimary=True)
+        contact1 = models.Contact(entityFK=entity1.pk, type=models.TYPE_PHONE,
+                                  value='(204) 555-1234', isprimary=True)
+        org1 = models.Organization(entityFK=entity1.pk, name='Test Org',
+                            description='This is a test organization')
+
+        """ Add the relationships """
+        entity1.addresses = [address1]
+        entity1.contacts = [contact1]
+        org1.entity = entity1
         
+        """ Add the data objects """       
+        self.db.session.add(org1)
+        self.db.session.commit()
+
+        """ Retrieve the organization and test that data matches """
+        org2 = models.Organization.query.filter_by(name='Test Org').first()
+        self.assertEquals(org2, org1)
+        self.assertEquals(org2.entity, entity1)
+        self.assertEquals(org2.entity.addresses[0], address1)
+        self.assertEquals(org2.entity.contacts[0], contact1)
+
+        """ Try grabbing some of the sub-objects from the database """
+        entity2 = models.Entity.query.filter_by(pk=org2.entity.pk).first()
+        self.assertEquals(entity2, entity1)
+
+        address2 = models.Address.query.filter_by(pk=org2.entity.addresses[0].pk).first()
+        self.assertEquals(address2, address1)
+
+        contact2 = models.Contact.query.filter_by(pk=org2.entity.contacts[0].pk).first()
+        self.assertEquals(contact2, contact1)
+
+    def test_organization_delete(self):
+        """ Delete Organization 1 """
+        org1 = models.Organization.query.filter_by(entityFK=1).first()
+        self.db.session.delete(org1)
+        self.db.session.commit()
+
+        """ Test that it has been deleted """
+        org2 = models.Organization.query.filter_by(entityFK=1).first()
+        self.assertEquals(org2, None)
+
+        """ Check if the first entity is no longer entity.pk==1
+            This will determine if entity was auto-deleted and that
+            other entities still exist """
+        entity1 = models.Entity.query.first()
+        self.assertNotEquals(entity1, None)
+        self.assertNotEquals(entity1.pk, 1)
+
+        """ Test that the addresses and contacts have been deleted """
+        address1 = models.Address.query.filter_by(entityFK=1).first()
+        contact1 = models.Contact.query.filter_by(entityFK=1).first()
+        self.assertEquals(address1, None)
+        self.assertEquals(contact1, None)
+
 if __name__ == "__main__":
     unittest.main()
