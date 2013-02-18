2be9b63c1a33861d3922f1ee6bb1e0b1d58c85e7
Diana <diana@diana-Aspire-one.(none)>
2013-02-17 22:38:40 -0600

    Committing so I can pull

--- ESA/application.py
+++ ESA/application.py
@@ -5,5 +5,9 @@ from ESA import app
 def home():
     return render_template('index.html')
 
+@app.route('/register_organization.html/')
+def home2():
+    return render_template('register_organization.html')
+
 if __name__ == '__main__':
     app.run()
