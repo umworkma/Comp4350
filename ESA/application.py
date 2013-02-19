6b6512e50c7ec1ec9941e6ed8726e68a3eab70a4
William Sung <umsung@cc.umanitoba.ca>
2013-02-19 13:01:06 -0600

    Refactor/Fixing up HTML and JS code before adding qunit test

--- ESA/application.py
+++ ESA/application.py
@@ -16,20 +16,27 @@ def home():
 def register_organization():
     return render_template('register_organization.html')
 
-@app.route('/_submit_org_form/')
+@app.route('/_submit_org_form', methods=['GET', 'POST'])
 def submit_org_form():
-    #get all user information
-    orgname = request.args.get('orgname')
-    desc = request.args.get('desc')
-    pwd = request.args.get('pwd')
-    phone = request.args.get('phone')
-    address = request.args.get('address')
-    city = request.args.get('city')
-    province = request.args.get('province')
-    postal = request.args.get('postal')
-    email = request.args.get('email')
-    #create a jason object and send it back to client 
-    return jsonify(orgname=orgname, desc=desc, pwd=pwd, phone=phone, address=address, city=city, province=province, postal=postal, email=email)
+    if request.method == 'POST':
+        #get all user information
+        orgname  = request.form['orgname']
+        desc     = request.form['desc']
+        pwd      = request.form['pwd']
+        phone    = request.form['phone']
+        address  = request.form['address']
+        city     = request.form['city']
+        province = request.form['province']
+        postal   = request.form['postal']
+        email    = request.form['email']
+
+        #create a jason object and send it back to client 
+        # return jsonify(request.form)
+        return jsonify(orgname=orgname, desc=desc, pwd=pwd, phone=phone, address=address, city=city, province=province, postal=postal, email=email)
+
+    else:
+        return jsonify(msg='Other request method[%s]' % request.method)
+
 
 @app.teardown_request
 def shutdown_session(exception=None):
---
+++ ESA/static/jClient/ESA-client.js
@@ -0,0 +1,35 @@
+// create json object and send it to server
+function createJsonObject() {
+    data = {
+        orgname: $('input[name="orgname"]').val(),
+        desc: $('#desc').val(),
+        pwd: $('input[name="pwd"]').val(),
+        phone: $('input[name="phone"]').val(),
+        address: $('input[name="address"]').val(),
+        city: $('input[name="city"]').val(),
+        province: $('input[name="province"]').val(),
+        postal: $('input[name="postal"]').val(),
+        email: $('input[name="email"]').val()
+    },
+
+    success = function(data) {
+        // get a value which sent back from server
+        alert("successfully submitted"
+                +"\n ***** Your Info *****"
+                +"\n user name: " + data.orgname
+                +"\n description: " + data.desc
+                +"\n password: " + data.pwd
+                +"\n phone: " + data.phone
+                +"\n address: " + data.address
+                +"\n city: " + data.city
+                +"\n province: " + data.province
+                +"\n postal: " + data.postal
+                +"\n email: " + data.email);
+
+    },
+
+    // ajax post request
+    $.post($SCRIPT_ROOT + '/_submit_org_form', data, success, "json");
+
+    return false;
+}
