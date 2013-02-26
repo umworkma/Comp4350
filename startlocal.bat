a5954a7b0d57d356dc61fa420527e421804c9113
Chris Workman <umworkma@cc.umanitona.ca>
2013-02-26 08:58:52 -0600

    Added some additional batch files for easy execution of all tests and starting of server with local database files.

---
+++ startlocal.bat
@@ -0,0 +1,8 @@
+@echo off
+REM This batch file is in place to help simplify running the server locally
+REM It will run the manage.py file using the python located in the venv folder
+REM of your project source. This should be easily executed by simply double
+REM clicking the batch file to run it.
+
+venv\Scripts\python.exe manage.py runlocal
+@echo on
