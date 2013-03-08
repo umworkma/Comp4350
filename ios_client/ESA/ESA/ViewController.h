f41cb6226716e372a654b429bc98f3cf53098d4d
umworkma <umworkma@cc.umanitoba.ca>
2013-03-08 14:51:01 -0600

    Starting work on iOS organization list

---
+++ ios_client/ESA/ESA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ESA
+//
+//  Created by ShiKage on 2013-03-08.
+//  Copyright (c) 2013 Comp4350 Group 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ios_client/ESA/ESA/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ESA
+//
+//  Created by ShiKage on 2013-03-08.
+//  Copyright (c) 2013 Comp4350 Group 1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
