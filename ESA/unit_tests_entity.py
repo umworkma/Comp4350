f5c540d48adc635d28e5978e6cf4823275a4db6d
Diana <diana@diana-ThinkPad-T430u.(none)>
2013-03-11 15:37:02 -0500

    Dan fixed a unit test.

--- ESA/unit_tests_entity.py
+++ ESA/unit_tests_entity.py
@@ -95,7 +95,7 @@ class EntityTestCase(TestCase):
         # Define prerequisite data.
         entityKey = 3
         # Retrieve the target object directly.
-        directList = models.Contact.query.filter_by(entityFK=entityKey)
+        directList = models.Contact.query.filter_by(entityFK=entityKey).order_by(models.Contact.isprimary, models.Contact.type)
         self.assertIsNotNone(directList)
         # Retrieve the containing object.
         host = models.Entity.query.filter_by(pk=entityKey).first()
